99f924a9afd48839daeb02857b1fe38da2e81806
Sol <sol6801@gmail.com>
2024-09-10 18:07:18 -0300

    Edit routes from categories and [groupId]

--- proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/categories/page.js
+++ proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/categories/page.js
@@ -1,10 +1,21 @@
+'use client'
+
 import React from 'react';
 import CategoryLayout from './layout';
+import { useRouter } from 'next/navigation';
 
-const categories = () => {
+
+const categories = ({ params }) => {
+  const router = useRouter();
+  const { groupId } = params; 
+
+  const goBack = () => {
+    router.back(); // Esto te permitirá volver a la página anterior
+  };
 
   return (
     <section className='bg-violet-400 grid place-items-center flex-1 rounded-lg'>
+      <h1 className="text-3xl font-bold text-center mb-12">Categories del Grupo {groupId}</h1>
       <section className="py-20 bg-gray-100 rounded-lg">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-center mb-12">Empezá a elegir dentro de cada categoría.</h2>
@@ -29,6 +40,11 @@ const categories = () => {
           </div>
         </div>
       </section>
+      <button onClick={goBack}>
+      <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
+            Ir Atrás
+          </a>
+      </button>
 
     </section>
   );
--- proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/page.js
+++ proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/page.js
@@ -1,11 +1,31 @@
+'use client';
+import Friend from "@/app/components/friend";
+import { useRouter } from 'next/navigation';
+
 const GroupPage = ({ params, children }) => {
+  const router = useRouter();
+  const { groupId } = params;
+
+  const goToCategories = () => {
+    router.push(`/groups/${groupId}/categories`);
+  };
 
   return (
-    <section className='bg-violet-400 grid place-items-center flex-1 rounded-lg'>
+    <section className='bg-violet-400 grid flex-1 rounded-lg'>
 
       <article>
-        <h2>Group Id:{params.groupId} Page </h2>
-        
+        <h2 className="text-3xl font-bold text-center mb-12">Group Id:{params.groupId} Page </h2>
+        <section>
+          <h1 className="text-3xl font-bold text-center mb-12">Miembros:</h1>
+
+          <Friend/>
+          <button onClick={goToCategories}>  
+          <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
+            Categorias
+          </a>
+          </button>
+        </section>
+
 
       {children}
       </article>
