390ca69165049ccd0eb0458ac4d83776973acc13
Sol <sol6801@gmail.com>
2024-09-10 18:50:51 -0300

    Add go back button to meals, places and movies

--- proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/categories/movies/page.js
+++ proyecto-final/src/app/(pages)/(friends)/groups/[groupId]/categories/movies/page.js
@@ -1,11 +1,26 @@
+'use client';
 import SwipeableCard from "@/app/components/swipeablecard";
-import Image from "next/image";
+import { useRouter } from 'next/navigation';
+// import Image from "next/image";
 
-export default function Movies() {
+const Movies = () => {
+  const router = useRouter();
+
+  const goBack = () => {
+    router.back(); 
+  };
   return (
     <section className='w-screen flex flex-col bg-violet-400 rounded-lg'>
      <h1 className="text-3xl font-bold text-center mb-12">Peliculas</h1>
    <SwipeableCard/>
+   
+   <button onClick={goBack}>
+      <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
+            Ir Atrás
+          </a>
+      </button>
    </section>
   );
 }
+
+export default Movies;
