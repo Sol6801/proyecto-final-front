7b524768b1232b9c887caa547063b140187e70d4
Sol <sol6801@gmail.com>
2024-09-23 14:09:45 -0300

    Fix member list functionality

--- src/app/(pages)/(events)/events/[eventId]/page.js
+++ src/app/(pages)/(events)/events/[eventId]/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Member from "@/components/member.js";
-import withAuth from '@/components/withAuth.js'
+import withAuth from "@/components/withAuth.js";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -11,42 +11,37 @@ const EventPage = ({ params }) => {
   const router = useRouter();
   const [eventUsers, setEventUsers] = useState([]);
 
-
   const goToCategories = () => {
     router.push(`/events/${eventId}/categories`);
   };
 
-
-
   useEffect(() => {
     const fetchEventUsers = async () => {
-      const eventId = localStorage.getItem('eventId');
-      if (!eventId) {
-        console.error('Event ID not found in local storage');
-        return;
-      }
+      // const eventId = localStorage.getItem('eventId');
+      // if (!eventId) {
+      //   console.error("Event ID not found in local storage");
+      //   return;
+      // }
 
       try {
         const response = await fetch(`${API_URL}/events/${eventId}/users`);
         const result = await response.json();
-        console.log('Event users:', result);
+        console.log("Event users:", result);
 
         if (Array.isArray(result.data)) {
-          const users = result.data.map(item => ({
+          const users = result.data.map((item) => ({
             id: item.user.id,
-            name: item.user.username,
-            eventId,
+            username: item.user.username
           }));
           setEventUsers(users);
         }
       } catch (error) {
-        console.error('Error fetching event users:', error);
+        console.error("Error fetching event users:", error);
       }
     };
 
     fetchEventUsers();
   }, []);
-
 
   const handleReady = async () => {
     try {
@@ -64,8 +59,8 @@ const EventPage = ({ params }) => {
           itemImage: currentItem.imageUrl,
           category: category,
         }),
-      });      
-      
+      });
+
       const data = await response.json();
       console.log(`${category} liked:`, data);
       handleNext();
@@ -74,7 +69,6 @@ const EventPage = ({ params }) => {
       console.error("Error details:", error.message);
     }
   };
-
 
   return (
     <section className="h-screen bg-violet-400 grid flex-1 rounded-lg">
@@ -86,24 +80,39 @@ const EventPage = ({ params }) => {
           <h2 className="text-3xl font-bold text-center mb-12">Miembros:</h2>
 
           <div style={gridStyle}>
-            {eventUsers.map((user) => (
-              <Member
-                key={user.id}
-                name={user.username}
-                imageUrl={user.imageUrl}
-              />
-            ))}
+
+          <nav>
+            <ul className="flex flex-col gap-10">
+              {eventUsers.length === 0 ? (
+                <p>No tienes miembros disponibles.</p>
+              ) : (
+                eventUsers.map((user) => (
+                  <li key={user.id}
+                      className="text-3xl bg-violet-300 hover:bg-violet-400 text-black font-bold py-2 px-4 rounded"
+                    >
+                      {user.username}
+                  </li>
+                ))
+              )}
+            </ul>
+          </nav>
           </div>
 
           <h2 className="text-3xl font-bold text-center mb-12">
             Empezá a planear!
           </h2>
 
-          <button onClick={goToCategories}className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-              Categorias
+          <button
+            onClick={goToCategories}
+            className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100"
+          >
+            Categorias
           </button>
-          <button onClick={handleReady} className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-              Listo!
+          <button
+            onClick={handleReady}
+            className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100"
+          >
+            Listo!
           </button>
         </section>
       </article>
