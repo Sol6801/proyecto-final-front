9b1561955da5feb01ace6a84c8cf8a37873554b8
Sol <sol6801@gmail.com>
2024-09-24 00:58:18 -0300

    Fix endpoint in swipeable-card for like, dislike

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-<<<<<<< HEAD
         protocol: "https",
         hostname: "randomuser.me",
         pathname: "/api/portraits/**",
-=======
-        protocol: 'https',
-        hostname: 'randomuser.me',
-        pathname: '/**',
->>>>>>> 7d73851b74e06de3760bf75b62bc95a639601402
       },
       {
         protocol: "https",
@@ -49,28 +43,23 @@ const nextConfig = {
         pathname: "/**",
       },
       {
-<<<<<<< HEAD
         protocol: "https",
         hostname: "static.vecteezy.com",
         pathname: "/**",
-=======
         protocol: 'https',
         hostname: 'm.media-amazon.com',
         pathname: '/images/**',
->>>>>>> 7d73851b74e06de3760bf75b62bc95a639601402
       },
       {
-<<<<<<< HEAD
         protocol: 'https',
         hostname: 'cdn-icons-png.flaticon.com',
         pathname: '/**',
       },
-=======
+      {
         protocol: "https",
         hostname: "cdn-icons-png.flaticon.com",
         pathname: "/**",
       },
-<<<<<<< HEAD
       {
         protocol: "https",
         hostname: "image.tmdb.org",
@@ -121,9 +110,11 @@ const nextConfig = {
         hostname: "www.buenosairesfreewalks.com",
         pathname: "/**",
       },
-=======
->>>>>>> 7d73851b74e06de3760bf75b62bc95a639601402
->>>>>>> ec9b86bb48c1ca68f095c721ed646caffb661fe4
+        {
+          protocol: "https",
+          hostname: "dynamic-media-cdn.tripadvisor.com",
+          pathname: "/media/**",
+        },
     ],
   },
 };
