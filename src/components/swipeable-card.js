57925e65b3fede224cd3b2e7cd322257cd599bfc
Sol <sol6801@gmail.com>
2024-09-25 17:31:35 -0300

    Fix index issue from swipeable card, add storeIndx

--- src/components/swipeable-card.js
+++ src/components/swipeable-card.js
@@ -1,32 +1,36 @@
-"use client"; // Asegúrate de que esto es un componente del cliente
+"use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSwipeable } from "react-swipeable";
 import useUserStore from "@/store/useUserStore.js";
+import useCategoryIndexStore from "@/store/useCategoryIndexStore.js";
 import Image from "next/image";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const SwipeableCard = ({ items, category, eventId }) => {
   const { userId } = useUserStore();
-  
-  console.log(eventId);
-  
+  const { setIndex, getIndex } = useCategoryIndexStore();
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  useEffect(() => {
+    const savedIndex = getIndex(eventId, category);
+    setCurrentIndex(savedIndex);
+  }, [eventId, category, getIndex]);
+
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+    const newIndex = (currentIndex + 1) % items.length;
+    setCurrentIndex(newIndex);
+    setIndex(eventId, category, newIndex);
   };
 
   const handleLike = async () => {
     try {
-      // const token = localStorage.getItem('token');
       const currentItem = items[currentIndex];
       const response = await fetch(`${API_URL}/events/${eventId}/${category}/liked`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify({
           userId,
@@ -49,13 +53,11 @@ const SwipeableCard = ({ items, category, eventId }) => {
 
   const handleDislike = async () => {
     try {
-      // const token = localStorage.getItem('token');
       const currentItem = items[currentIndex];
       const response = await fetch(`${API_URL}/events/${eventId}/${category}/disliked`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // "Authorization": `Bearer ${token}`
         },
         body: JSON.stringify({
           userId,
@@ -76,7 +78,6 @@ const SwipeableCard = ({ items, category, eventId }) => {
     }
   };
 
-  // Configura los manejadores de swipe
   const handlers = useSwipeable({
     onSwipedLeft: handleDislike,
     onSwipedRight: handleLike,
@@ -84,9 +85,8 @@ const SwipeableCard = ({ items, category, eventId }) => {
     trackMouse: true,
   });
 
-  // Verifica que haya elementos y que el índice sea válido
   if (items.length === 0 || !items[currentIndex]) {
-    return <h2>No hay elementos disponibles para mostrar.</h2>; //*crear loader */
+    return <h2>No hay elementos disponibles para mostrar.</h2>;
   }
 
   return (
@@ -100,10 +100,9 @@ const SwipeableCard = ({ items, category, eventId }) => {
         {...handlers}
         className="flex flex-col justify-evenly min-w-96 h-full min-h-80 items-center bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden cursor-pointer"
       >
-        {/* Verifica que items[currentIndex] tenga las propiedades necesarias */}
         <Image
           className="object-cover"
-          src={items[currentIndex].urlImage || "/fallback-image.jpg"} // Usa una imagen de respaldo si no existe
+          src={items[currentIndex].urlImage || "/fallback-image.jpg"}
           alt={items[currentIndex].name || "Imagen no disponible"}
           width={400} 
           height={800}
@@ -126,139 +125,3 @@ const SwipeableCard = ({ items, category, eventId }) => {
 };
 
 export default SwipeableCard;
-
-
-// "use client"; // Asegúrate de que esto es un componente del cliente
-
-// import React, { useState, useEffect } from "react";
-// import { useSwipeable } from "react-swipeable";
-// import useUserStore from "@/store/useUserStore.js";
-// import useEventStore from "@/store/useEventStore.js";
-// import Image from "next/image"; // Importa el componente de Next.js
-
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-// const SwipeableCard = ({ items, category, eventId }) => {
-//   const { userId } = useUserStore();
-//   const [currentIndex, setCurrentIndex] = useState(0);
-  
-
-//   // Dependiendo de la categoría, selecciona el store correspondiente
-//   const setIndex = category === 'movies' ? useMovieIndexStore((state) => state.setIndex) :
-//                  category === 'places' ? usePlaceIndexStore((state) => state.setIndex) :
-//                  useMealIndexStore((state) => state.setIndex);
-
-//   const getIndex = category === 'movies' ? useMovieIndexStore((state) => state.getIndex(eventId)) :
-//                   category === 'places' ? usePlaceIndexStore((state) => state.getIndex(eventId)) :
-//                   useMealIndexStore((state) => state.getIndex(eventId));
-
-//                   useEffect(() => {
-//                     setCurrentIndex(getIndex); // Inicializa el índice al cargar
-//                   }, [eventId, getIndex]);
-                
-//                   const handleNext = () => {
-//                     const newIndex = (currentIndex + 1) % items.length;
-//                     setCurrentIndex(newIndex);
-//                     setIndex(eventId, newIndex); // Actualiza el índice en el store
-//                   };
-
-//   const handleLike = async () => {
-//     try {
-//       const currentItem = items[currentIndex];
-//       const response = await fetch(`${API_URL}/${category}/liked`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           userId,
-//           itemId: currentItem.id,
-//           itemName: currentItem.name,
-//           itemImage: currentItem.urlImage,
-//           category: category,
-//         }),
-//       });
-
-//       const data = await response.json();
-//       console.log(`${category} liked:`, data);
-//       handleNext();
-//     } catch (error) {
-//       console.error(`Error saving ${category} like:`, error);
-//     }
-//   };
-
-//   const handleDislike = async () => {
-//     try {
-//       const currentItem = items[currentIndex];
-//       const response = await fetch(`${API_URL}/${category}/disliked`, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           userId,
-//           itemId: currentItem.id,
-//           itemName: currentItem.name,
-//           itemImage: currentItem.urlImage,
-//           category: category,
-//         }),
-//       });
-
-//       const data = await response.json();
-//       console.log(`${category} disliked:`, data);
-//       handleNext();
-//     } catch (error) {
-//       console.error(`Error saving ${category} dislike:`, error);
-//     }
-//   };
-
-//   // Configura los manejadores de swipe
-//   const handlers = useSwipeable({
-//     onSwipedLeft: handleDislike,
-//     onSwipedRight: handleLike,
-//     preventDefaultTouchmoveEvent: true,
-//     trackMouse: true,
-//   });
-
-//   // Verifica que haya elementos y que el índice sea válido
-//   if (items.length === 0 || !items[currentIndex]) {
-//     return <h2>No hay elementos disponibles para mostrar.</h2>;
-//   }
-
-//   return (
-//     <section className="flex flex-row justify-around">
-//       <button onClick={handleDislike}>
-//         <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-//           Dislike
-//         </a>
-//       </button>
-//       <div
-//         {...handlers}
-//         className="w-full max-w-sm mx-auto h-96 bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden cursor-pointer"
-//       >
-//         {/* Usar el componente Image de Next.js */}
-//         <Image
-//           className="w-full h-80 object-cover"
-//           src={items[currentIndex].urlImage || "/fallback-image.jpg"}
-//           alt={items[currentIndex].name || "Imagen no disponible"}
-//           width={500}
-//           height={400}
-//         />
-
-//         <div className="p-4">
-//           <h3 className="text-xl font-bold mb-2">
-//             {items[currentIndex].name || "Título no disponible"}
-//           </h3>
-//         </div>
-//       </div>
-
-//       <button onClick={handleLike}>
-//         <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-//           Like
-//         </a>
-//       </button>
-//     </section>
-//   );
-// };
-
-// export default SwipeableCard;
