29c4d8b3aa07e42e6476e34d4ebcbe4f4c9407e1
Sol <sol6801@gmail.com>
2024-09-26 16:01:54 -0300

    Add image to create and join event, add styles

--- src/app/(pages)/(events)/events/page.js
+++ src/app/(pages)/(events)/events/page.js
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import withAuth from '@/components/withAuth.js'
+import withAuth from "@/components/withAuth.js";
+import Image from "next/image";
 
 const Events = () => {
   const router = useRouter();
@@ -17,19 +18,34 @@ const Events = () => {
   return (
     <section className=" bg-violet-400 place-items-center flex-1 rounded-lg h-full min-h-screen grid">
       <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
-        <button onClick={createEvent}>
-          <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-            Crear Evento
-          </a>
-        </button>
-        <button onClick={joinEvent}>
-          <a className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
-            Unirse a Evento
-          </a>
-        </button>
+        <div className="flex flex-col items-center justify-center gap-8">
+          <Image
+            src="https://cdn-icons-png.freepik.com/512/5948/5948841.png?uid=R133653469&ga=GA1.1.413291434.1727374790"
+            alt="Imagen Crear Evento"
+            width={150}
+            height={150}
+            className="rounded-lg"
+          />
+
+          <button onClick={createEvent} className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
+              Crear Evento
+          </button>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-8">
+          <Image
+            src="https://cdn-icons-png.freepik.com/512/5948/5948841.png?uid=R133653469&ga=GA1.1.413291434.1727374790"
+            alt="Imagen Crear Evento"
+            width={150}
+            height={150}
+            className="rounded-lg"
+          />
+          <button onClick={joinEvent}  className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-100">
+              Unirse a Evento
+          </button>
+        </div>
       </div>
     </section>
-  );
+  );s
 };
 
 export default withAuth(Events);
