1f86a9e584cff60846c60a588b4089e32c9ebf4b
Martín Miselli <martin.miselli@gmail.com>
2024-09-26 19:23:32 -0300

    loader changes

--- src/app/(pages)/(events)/events/layout.js
+++ src/app/(pages)/(events)/events/layout.js
@@ -8,10 +8,12 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 function EventsLayout({ children, createEventModal, joinEventModal }) {
   const [userEvents, setUserEvents] = useState([]);
+  const [loader, setLoader] = useState(true)
   const router = useRouter();
 
   useEffect(() => {
     const fetchUserEvents = async () => {
+      setLoader(true)
       const userId = localStorage.getItem('userId');
       if (!userId) {
         console.error('User ID not found in local storage');
@@ -28,6 +30,7 @@ function EventsLayout({ children, createEventModal, joinEventModal }) {
             id: item.event.id,
             name: item.event.name
           }));
+          setLoader(false)
           setUserEvents(events);
         }
       } catch (error) {
@@ -45,30 +48,46 @@ function EventsLayout({ children, createEventModal, joinEventModal }) {
   return (
     <>
       <Navbar />
-      {userEvents.length === 0 ? (
-        <div className="bg-violet-400 place-items-center flex-1 flex flex-col h-full min-h-screen items-center">
-          <section className=" py-10 text-center">
-              <h2 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-white mb-4 drop-shadow-lg">
-                Todavía no tenés eventos!
-              </h2>
-              <h3 className="text-xl sm:text-2xl md:text-3xl text-white font-semibold opacity-90 mb-6">
-                Empezá a planear ya!
-              </h3>
+
+      {loader && (
+        <div className="bg-gradient-to-b from-violet-500 to-violet-200 place-items-center flex-1 flex flex-col h-full min-h-screen items-center">
+          <section className="py-10 text-center">
+            <div className="w-full h-screen flex justify-center items-center min-h-full">
+              <div className="relative w-16 h-16">
+                <div className="w-full h-full border-4 border-purple-900 border-t-transparent rounded-full animate-spin"></div>
+                <div className="absolute inset-0 m-auto w-12 h-12 border-4 border-purple-500 border-t-transparent rounded-full animate-spin-slow"></div>
+                <div className="absolute inset-0 m-auto w-8 h-8 border-4 border-purple-300 border-t-transparent rounded-full animate-spin-reverse"></div>
+              </div>
+            </div>
           </section>
-        {children}
-        {createEventModal}
-        {joinEventModal}
-      </div>
-      ) : (
-        <div className="flex flex-col bg-gray-100 mx-auto p-4 gap-4 h-full min-h-screen md:flex-row">
-        <aside className="bg-violet-600 md:max-w-80 px-20 grid place-items-center rounded-lg relative order-1 md:order-0">
-          <span className="absolute top-4 left-4">
-            <h1 className="text-xl p-1 text-center">Selecciona un evento para verlo</h1>
-          </span>
-          <nav>
-            <ul className="flex flex-col py-10 my-5 gap-10 ">
-              {
-              userEvents.map((event) => (
+        </div>
+      )}
+
+      {!loader && userEvents.length === 0 && (
+        <div className="bg-violet-400 place-items-center flex-1 flex flex-col h-full min-h-screen items-center">
+          <section className="py-10 text-center">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl font-extrabold text-white mb-4 drop-shadow-lg">
+              ¡Todavía no tienes eventos!
+            </h2>
+            <h3 className="text-xl sm:text-2xl md:text-3xl text-white font-semibold opacity-90 mb-6">
+              ¡Empieza a planear ya!
+            </h3>
+          </section>
+          {children}
+          {createEventModal}
+          {joinEventModal}
+        </div>
+      )}
+
+      {!loader && userEvents.length > 0 && (
+        <div className="flex flex-col bg-gradient-to-b from-violet-500 to-violet-200 mx-auto p-4 gap-4 h-full min-h-screen md:flex-row">
+          <aside className="bg-violet-600 md:max-w-80 px-20 grid place-items-center rounded-lg relative order-1 md:order-0">
+            <span className="absolute top-4 left-4">
+              <h1 className="text-xl p-1 text-center">Selecciona un evento para verlo</h1>
+            </span>
+            <nav>
+              <ul className="flex flex-col py-10 my-5 gap-10">
+                {userEvents.map((event) => (
                   <li key={event.id}>
                     <button
                       onClick={() => handleEventClick(event.id)}
@@ -77,15 +96,14 @@ function EventsLayout({ children, createEventModal, joinEventModal }) {
                       {event.name}
                     </button>
                   </li>
-                ))
-              }
-            </ul>
-          </nav>
-        </aside>
-        <div className="bg-violet-400 place-items-center flex-1 flex items-center rounded-lg order-0 md:order-1">
-          {children}
+                ))}
+              </ul>
+            </nav>
+          </aside>
+          <div className="bg-violet-400 place-items-center flex-1 flex items-center rounded-lg order-0 md:order-1">
+            {children}
+          </div>
         </div>
-      </div>
       )}
       
         
--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -23,7 +23,7 @@ const withAuth = (WrappedComponent) => {
 
         if (isLoading) {
             return (
-                <div className="w-full h-screen flex justify-center items-center min-h-full">
+                <div className="bg-gradient-to-b from-violet-500 to-violet-200 w-full h-screen flex justify-center items-center min-h-full">
                 <div className="relative w-16 h-16">
                   <div className="w-full h-full border-4 border-purple-900 border-t-transparent rounded-full animate-spin"></div>
                     <div className="absolute inset-0 m-auto w-12 h-12 border-4 border-purple-500 border-t-transparent rounded-full animate-spin-slow"></div>
