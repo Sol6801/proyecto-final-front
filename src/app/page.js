77a1b871fe9f38f0508332088460146199bfef65
Sol <sol6801@gmail.com>
2024-10-01 10:56:55 -0300

    Add description to landing

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,12 @@ const LandingPage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Main Content */}
-      <main className="h-screen flex flex-col items-center justify-center py-20 bg-gradient-to-b from-violet-500 to-violet-200 text-white">
+      <main className="h-screen flex flex-col items-center justify-center py-20 bg-gradient-to-b from-violet-500 to-violet-200 text-white p-3">
         <h1 className="text-5xl font-extrabold mb-4 text-center">
           Planner Buddy
         </h1>
-        <p className="text-xl mb-8">Planeá tu salida mientras te divertis!</p>
+        <p className="text-lg text-center mb-5 md:mb-0">Da like o dislike en categorías como películas, lugares y comidas, y deja que Planner Buddy proponga la mejor opción según los gustos del grupo.</p>
+        <p className="text-lg mb-8 text-center md:px-28">¡Planner Buddy te ayuda a tomar decisiones grupales de forma divertida!</p>
 
         <Link href="/login" passHref>
           <button className="bg-white text-violet-600 px-6 py-3 rounded-full text-lg font-semibold hover:bg-gray-200 min-w-40">
